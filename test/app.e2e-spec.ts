f6ab1da58c0b5e5314bd0259d20355e126c9d03b
dzhanumian_artur <arthur114499@gmail.com>
2023-11-08 14:08:43 +0300

    fix tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,46 +1,67 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import {Test, TestingModule} from '@nestjs/testing';
+import {INestApplication} from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import {AppModule} from './../src/app.module';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
-  const accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RAZ21haWwuY29tIiwiaWF0IjoxNjk5Mjg3MzU1LCJleHAiOjE2OTkyOTA5NTV9.BrtmhMUt1mPnYcUIdHQTR4U5GAwx5QeEHRXWyiRNzt4'
+    let app: INestApplication;
+    const accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RAZ21haWwuY29tIiwiaWF0IjoxNjk5Mjg3MzU1LCJleHAiOjE2OTkyOTA5NTV9.BrtmhMUt1mPnYcUIdHQTR4U5GAwx5QeEHRXWyiRNzt4'
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+    beforeEach(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+        app = moduleFixture.createNestApplication();
+        await app.init();
+    });
 
-  it('/tasks (GET)', () => {
-    return request(app.getHttpServer())
-        .get('/tasks')
-        .set('Authorization', 'Bearer '+accessToken)
-        .expect(200);
-  });
+    let taskId;
 
-  it('/tasks (POST)', () => {
-    return request(app.getHttpServer())
-        .post('/tasks')
-        .send({ name: 'Новая задача' })
-        .set('Authorization', 'Bearer '+accessToken)
-        .expect(201)
-        .expect(res => {
-          expect(res.body).toEqual(
-              expect.objectContaining({
-                id: expect.any(Number),
-                name: expect.any(String),
-              }),
-          );
-        })
-  });
+    it('/tasks (GET)', () => {
+        return request(app.getHttpServer())
+            .get('/tasks')
+            .set('Authorization', 'Bearer ' + accessToken)
+            .expect(200);
+    });
 
-  afterAll((done) => {
-    app.close();
-    done();
-  });
+    it('/tasks (POST)', () => {
+        return request(app.getHttpServer())
+            .post('/tasks')
+            .send({name: 'Новая задача'})
+            .set('Authorization', 'Bearer ' + accessToken)
+            .expect(201)
+            .expect(res => {
+                expect(res.body).toEqual(
+                    expect.objectContaining({
+                        id: expect.any(Number),
+                        name: 'Новая задача',
+                    }),
+                );
+                taskId = res.body.id
+            })
+    });
+
+    it(`/tasks/${taskId} (GET)`, () => {
+        return request(app.getHttpServer())
+            .get(`/tasks/${taskId}`)
+            .expect(200)
+            .set('Authorization', 'Bearer ' + accessToken)
+            .expect({
+                id: taskId,
+                name: 'Новая задача'
+            });
+    });
+
+    it(`/tasks/${taskId} (DELETE)`, () => {
+        return request(app.getHttpServer())
+            .delete(`/tasks/${taskId}`)
+            .set('Authorization', 'Bearer ' + accessToken)
+            .expect(204);
+    });
+
+    afterAll((done) => {
+        app.close();
+        done();
+    });
 });
